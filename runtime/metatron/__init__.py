1c2abaff0392da130a2cb3c46438315563de3618
Heath Sanchez <32909488+heathsanchez@users.noreply.github.com>
2026-09-22 08:57:50 +1200

    refactor(nucleus): expose minimal warrant graph

--- runtime/metatron/__init__.py
+++ runtime/metatron/__init__.py
@@ -1,10 +1,13 @@
 from .model import ID_TABLE, STEP_TABLE, Query, Residual, State, Table
+from .nucleus import Node, WarrantGraph
 
 __all__ = [
     "ID_TABLE",
     "STEP_TABLE",
+    "Node",
     "Query",
     "Residual",
     "State",
     "Table",
+    "WarrantGraph",
 ]
