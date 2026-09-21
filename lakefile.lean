1d0d962fc3d8894804ced6b510cfaa0f25d10f06
Heath Sanchez <32909488+heathsanchez@users.noreply.github.com>
2026-09-22 09:26:34 +1200

    refactor(formal): remove Mathlib from minimal nucleus

--- lakefile.lean
+++ lakefile.lean
@@ -4,10 +4,6 @@ open Lake DSL
 package metatron where
   moreLeanArgs := #["-DautoImplicit=false"]
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @
-  "44ba35c6daa9d69aff8fed9fff9bbde17ded774d"
-
 @[default_target]
 lean_lib Metatron where
   srcDir := "formal"
